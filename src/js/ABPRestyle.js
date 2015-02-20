069b2ba8a3d152192eb1bd51c4189106a4a41480
swordfeng <swordfeng@outlook.com>
2015-02-20 13:09:37 +0800

    fix restyle

--- src/js/ABPRestyle.js
+++ src/js/ABPRestyle.js
@@ -1,11 +1,10 @@
-var ABP_Restyle = function() {/*
-	var playerUnits = document.getElementsByClassName("ABP-Unit");
-	for (var i=0;i<playerUnits.length;++i) {
-		var pUnit = playerUnits[i];
-		var pCommentList = pUnit.getElementsByClassName("ABP-CommentList-Container")[0];
-		pCommentList.onload = function() {
+var ABP_Restyle = function() {
+	setTimeout(function(){
+		var playerUnits = document.getElementsByClassName("ABP-Unit");
+		for (var i=0;i<playerUnits.length;++i) {
+			var pUnit = playerUnits[i];
+			var pCommentList = pUnit.getElementsByClassName("ABP-CommentList-Container")[0];
 			pCommentList.style.height = (pUnit.getBoundingClientRect().bottom-pCommentList.getBoundingClientRect().bottom-1)+"px";
 		};
-		pCommentList.onload();
-	};*/
+	},5000);
 };
