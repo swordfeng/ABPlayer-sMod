d9d245d59607b6c3563bd9b1461e53df0529259f
swordfeng <swordfeng@outlook.com>
2015-02-20 11:55:36 +0800

    update restyler

--- src/js/ABPRestyle.js
+++ src/js/ABPRestyle.js
@@ -3,7 +3,7 @@ var ABP_Restyle = function() {
 	for (var i=0;i<playerUnits.length;++i) {
 		var pUnit = playerUnits[i];
 		var pCommentList = pUnit.getElementsByClassName("ABP-CommentList-Container")[0];
-		pCommentList.onload=function() {
+		pCommentList.onload = function() {
 			pCommentList.style.height = (pUnit.getBoundingClientRect().bottom-pCommentList.getBoundingClientRect().bottom-1)+"px";
 		};
 		pCommentList.onload();
